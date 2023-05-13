8549d8bfe783df799651e4d86f9c2766db809a53
ZiaurRehmanmalhi <ziamalhi1234@gmail.com>
2023-05-13 10:39:47 +0500

    add

--- shop/models.py
+++ shop/models.py
@@ -5,7 +5,7 @@ from django.db import models
 class Product(models.Model):
     product_id = models.AutoField
     product_name = models.CharField(max_length=50)
-    category = models.CharField(max_length=100)
+    category = models.CharField(max_length=50, default="")
     subcategory = models.CharField(max_length=50, default="")
     price = models.IntegerField(default=0)
     desc = models.CharField(max_length=300)
